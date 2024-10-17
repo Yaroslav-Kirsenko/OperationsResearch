24a92b1e1218ebacb7a3080a3cfd7aa9ebab0e0f
ludmi <ludmi@DESKTOP-DAL6N9M>
2024-10-17 17:03:03 +0300

    Добавьте файлы проекта.

---
+++ OperationsResearch/Dual/ColumAndRowsDual.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System.Reflection.Emit;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace OperationsResearch.Dual
+{
+    /// <summary>
+    /// Логика взаимодействия для ColumAndRowsDual.xaml
+    /// </summary>
+    public partial class ColumAndRowsDual : Window
+    {
+        public ColumAndRowsDual()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string textFromTextBox1 = textBox1.Text;
+            int value;
+
+            // Проверяем, является ли введённый текст числом
+            if (int.TryParse(textFromTextBox1, out value))
+            {
+
+                if (value > 0)
+                {
+                    ShowExample showExample = new ShowExample();
+                    showExample.UpdateLabelContent(textFromTextBox1);
+                    showExample.Show();
+
+                    this.Close();
+                }
+                else
+                {
+                    errorMessage.Content = "нуль, або меньше нуля не може бути!!!";
+                }
+            }
+            else
+            {
+                errorMessage.Content = "Введіть коректне число!!!";
+            }
+        }
+
+        private void Button_Up1(object sender, RoutedEventArgs e)
+        {
+            int value;
+            if (int.TryParse(textBox1.Text, out value)) // проверка корректности ввода
+            {
+                textBox1.Text = (value + 1).ToString(); // увеличение на 1 и преобразование обратно в строку
+            }
+            else
+            {
+                errorMessage.Content = "Введіть коректне число!!!";
+            }
+        }
+
+        private void Button_Down1(object sender, RoutedEventArgs e)
+        {
+            int value;
+            if (int.TryParse(textBox1.Text, out value)) // проверка корректности ввода
+            {
+                textBox1.Text = (value - 1).ToString(); // увеличение на 1 и преобразование обратно в строку
+            }
+            else
+            {
+                errorMessage.Content = "Введіть коректне число!!!";
+            }
+        }
+
+        private void Button_Up2(object sender, RoutedEventArgs e)
+        {
+            int value;
+            if (int.TryParse(textBox2.Text, out value)) // проверка корректности ввода
+            {
+                textBox2.Text = (value + 1).ToString(); // увеличение на 1 и преобразование обратно в строку
+            }
+            else
+            {
+                errorMessage.Content = "Введіть коректне число!!!";
+            }
+        }
+
+        private void Button_Down2(object sender, RoutedEventArgs e)
+        {
+            int value;
+            if (int.TryParse(textBox2.Text, out value)) // проверка корректности ввода
+            {
+                textBox2.Text = (value - 1).ToString(); // увеличение на 1 и преобразование обратно в строку
+            }
+            else
+            {
+
+                errorMessage.Content = "Введіть коректне число!!!";
+            }
+        }
+    }
+}
---
+++ OperationsResearch/Dual/ShowExample.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OperationsResearch.Dual
+{
+    /// <summary>
+    /// Логика взаимодействия для ShowExample.xaml
+    /// </summary>
+    public partial class ShowExample : Window
+    {
+        public ShowExample()
+        {
+            InitializeComponent();
+        }
+
+
+        public void UpdateLabelContent(string content)
+        {
+            // Предположим, что Label с именем "displayLabel"
+            displayLabel.Content = content;
+        }
+    }
+}
